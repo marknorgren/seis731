4bc239524290224bd0fe50313a7f6880332f13ab
markd <mark@marknorgren.com>
2011-09-26 21:01:25 -0500

    adding homework3 directory

---
+++ Homework3/ir1.py
@@ -0,0 +1,20 @@
+# Debug flag: True=print intermediate data structures
+DEBUG = True
+
+# hardcoded (for simplicity) tuple of documents to index
+documents = ('D1.txt','D2.txt','D3.txt')
+
+index={}	# dictionary key: (document, term), value: frequencies/weights
+for document in documents:
+	for line in open( document, 'r'):
+		terms = line.split()
+		for term in terms:
+			if term != '':		# Ignore null results from split at start/end of line
+				if not index.has_key( ( document, term.lower() ) ):
+					index[ ( document, term.lower() ) ] = 0
+				index[ ( document, term.lower() ) ] += 1
+
+if DEBUG:
+	print '\nRaw term count:'
+	for ( document, term ) in sorted( index.keys() ):
+		print document, term, index[ ( document, term ) ]
